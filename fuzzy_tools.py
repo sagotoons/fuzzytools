abc7dd6374cfd9002edeced97325c55514b592fc
Sacha Goedegebure <151992752+sagotoons@users.noreply.github.com>
2024-06-19 17:31:45 +0800

    Removed operator 3d Cursor to Focus Distance
    Was proud about this operator, but it seems not needed...

--- fuzzy_tools.py
+++ fuzzy_tools.py
@@ -1300,37 +1300,6 @@ class OBJECT_OT_rename_camera_alpha(Operator):
 
 
 # ------------------------------------------------------------------------
-#    OPERATOR - 3D Cursor to Focus Distance
-# ------------------------------------------------------------------------
-
-class OBJECT_OT_cursor_to_focus_distance(Operator):
-    """Set distance to 3D Cursor as camera focus distance"""
-    bl_idname = "object.cursor_to_focus_distance"
-    bl_label = "3D Cursor to Focus Distance"
-    bl_options = {'UNDO'}
- 
-    @classmethod
-    def poll(cls, context):
-        return context.object is not None and context.object.type == 'CAMERA'
-
-    def execute(self, context):
-        scene = context.scene
-        cam = context.active_object
-
-        # camera and 3dcursor location
-        cam_loc = cam.matrix_world.to_translation()
-        cursor_loc = scene.cursor.matrix.to_translation()
-
-        # calculate distance
-        length = math.dist(cursor_loc, cam_loc)
-
-        # set new focus distance
-        cam.data.dof.focus_distance = length
-
-        return {'FINISHED'}
-
-
-# ------------------------------------------------------------------------
 #    OPERATOR - Move Keyframes and Markers
 # ------------------------------------------------------------------------
 
@@ -1893,7 +1862,6 @@ classes = [
     VIEW3D_OT_set_active_camera,    
     MARKER_OT_camera_bind_new,
     OBJECT_OT_rename_camera_alpha,
-    OBJECT_OT_cursor_to_focus_distance,
     
     TRANSFORM_OT_keyframes_markers,
 
